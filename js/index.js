4530201760d16cfb52ceb94b73c5cf6c6aa1ac85
newtonjain <newton@user3-82-57.wireless.utoronto.ca>
2015-01-13 21:57:24 -0500

    Added a json object called gems

--- js/index.js
+++ js/index.js
@@ -1,20 +1,41 @@
 var app = angular.module('prep', []);
 
-var gem = {
-
+var gem = [
+{
 	name: 'newton jain is in the house',
 	age: '25',
 	id: '007',
 	instore: true,
-}
+
+},
+
+{
+	name: 'jacob si',
+	age: '24',
+	id: '001',
+	instore: true,
+},
+{
+	name: 'nadir hassan',
+	age: '34',
+	id: '654',
+	instore: true,
+},
+{
+	name: 'gautam',
+	age: '21',
+	id: '003',
+	instore: true,
+}];
 
 app.controller('build1', function($scope){
 
 $scope.var1 = " Hello World";
+$scope.var2 = [];
 $scope.var2 = gem;
 $scope.fun1 = function(){
 
-	alert("you have clicked this button congrats" + gem.name);
+	alert("you have clicked this button congrats" + JSON.stringify($scope.var2[1]));
 }
 
 });
